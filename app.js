39fbfbe3cb0481c492974fb0b100ba014a936252
unknown <petetrainerlsf@gmail.com>
2021-01-31 18:05:49 +0100

    Added contact form

--- app.js
+++ app.js
@@ -1115,3 +1115,23 @@ function tableContent(){
 
    // Reset Strony Do podstawowych ustawien
    showButtons();
+
+
+   //Contact Form
+   const name = document.getElementById('name')
+   const email = document.getElementById('email')
+   const name = document.getElementById('form')
+   const errorElement = document.getElementById('error')
+
+   form.addEventListener('submit', (e)=>{
+      let messages = []
+      if (name.value === "" || name.value == null){
+         messages.push('Name is required')
+      }
+
+      if(messages.length > 0){
+      e.preventDefault()
+      errorElement.innerText = messages.join(', ')
+      }
+      
+   })
