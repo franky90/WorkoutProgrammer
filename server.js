423817c46c220364d6b6385f45df3b600fcb67f3
Felipe Ferreira <felipedferreira11@gmail.com>
2021-02-04 13:42:00 -0500

    adding app middleware

--- server.js
+++ server.js
@@ -11,7 +11,27 @@ app.get("/", function(req,res){
 app.post("/api/form", function(req,res){
     const body = req.body
     res.status(200).send({body: body})
-})
+});
+
+
+(function SetApplicationMiddleware(App) {
+        // Do I need to add this?
+        // App.use(Cors())
+        // Already did this...
+        // App.use(express.urlencoded({ extended: true }))// Parse URL-encoded bodies
+        // App.use(express.json())
+        App.use(( req , res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+            if(req.method === 'OPTIONS') {
+                res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+                return res.status(200).json({})
+            }
+            next();
+        })
+
+})(app);
+
 app.listen(3000, function(){
     console.log(`Application started on port: ${port}`)
 })
